ad03dcd17a58b491679899be3a6abf10b9e47863
AdityaBrahmaSaiNarra <93022328+AdityaBrahmaSaiNarra@users.noreply.github.com>
2024-02-03 10:47:17 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,4 +4,7 @@ provider "aws" {
 resource "aws_instance" "sentinelenforcer" {
   instance_type = "t2.micro"
   ami           = "ami-05fb0b8c1424f266b"
+  tags = {
+    Name = "SentinelPolicyCheckTag"
+  }
 }
